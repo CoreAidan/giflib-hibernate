e94481678ca886a70819d0f586da3ca0056caf75
InfamousWolf <AidanMelendez@gmail.com>
2018-02-19 21:43:10 -0500

    Finished Implementing the UserService class

--- src/main/java/com/coreaidan/giflib/service/UserServiceImpl.java
+++ src/main/java/com/coreaidan/giflib/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.coreaidan.giflib.service;
 
+import com.coreaidan.giflib.dao.UserDao;
 import com.coreaidan.giflib.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -8,13 +10,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserDao userDao;
+
     @Override
     public User findByUserName(String username) {
-        return null;
+        return userDao.findByUserName(username);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        // Load user from the database (throw exception if not found)
+        User user = userDao.findByUserName(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        //Return user object
+        return user;
     }
 }
