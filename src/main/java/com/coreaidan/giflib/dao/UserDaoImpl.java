5fcb6c2881804234221b9685a385dcadf035b06f
InfamousWolf <AidanMelendez@gmail.com>
2018-02-20 00:27:43 -0500

    Finished The main portion of the user authentication and added a login page also moved all static assets into a respective folder

--- src/main/java/com/coreaidan/giflib/dao/UserDaoImpl.java
+++ src/main/java/com/coreaidan/giflib/dao/UserDaoImpl.java
@@ -6,6 +6,11 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 @Repository
 public class UserDaoImpl implements UserDao {
 
@@ -14,9 +19,29 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User findByUserName(String username) {
+        // Open a session
         Session session = sessionFactory.openSession();
-        User user = session.get(User.class, username);
+
+        // DEPRECATED as of Hibernate 5.2.0
+        // List<Category> categories = session.createCriteria(Category.class).list();
+
+        // Create CriteriaBuilder
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        // Create CriteriaQuery
+        CriteriaQuery<User> criteria = builder.createQuery(User.class);
+
+        // Specify criteria root
+        Root<User> root = criteria.from(User.class);
+        Predicate condition = builder.equal(root.get("username"), username);
+        criteria.select(root).where(condition);
+
+        // Execute query
+        User user = session.createQuery(criteria).getSingleResult();
+
+        // Close session
         session.close();
+
         return user;
     }
 }
